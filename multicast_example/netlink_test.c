319d7d95c33ea232782aa91b233fb2a1aa54a8a0
Moritz Warning <moritzwarning@web.de>
2020-06-09 02:27:23 +0200

    update multicast code

--- multicast_example/netlink_test.c
+++ multicast_example/netlink_test.c
@@ -1,3 +1,4 @@
+#if 0
 #define MAX_PAYLOAD 1024
 struct sock *nl_sk = NULL;
 
@@ -6,8 +7,7 @@ void netlink_test() {
   struct nlmsghdr *nlh;
   int err;
 
-  nl_sk = netlink_kernel_create(NETLINK_TEST,
-                               nl_data_ready);
+  nl_sk = netlink_kernel_create(NETLINK_TEST, nl_data_ready);
   skb=alloc_skb(NLMSG_SPACE(MAX_PAYLOAD),GFP_KERNEL);
   nlh = (struct nlmsghdr *)skb->data;
   nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
@@ -25,3 +25,77 @@ void netlink_test() {
   netlink_broadcast(nl_sk, skb, 0, 1, GFP_KERNEL);
   sock_release(nl_sk->socket);
 }
+#endif
+
+
+#include <net/sock.h>
+#include <linux/module.h>
+#include <linux/netlink.h>
+#include <linux/skbuff.h>
+
+#define NETLINK_TEST 17
+
+
+struct sock *nl_sock = NULL;
+
+static void netlink_test_recv_msg(struct sk_buff *skb)
+{
+    struct sk_buff *skb_out;
+    struct nlmsghdr *nlh;
+    int msg_size;
+    char *msg;
+    int pid;
+    int res;
+
+    nlh = (struct nlmsghdr *)skb->data;
+    msg = (char *)nlmsg_data(nlh);
+    msg_size = strlen(msg);
+
+    printk(KERN_INFO "netlink_test: Received %s\n", msg);
+    pid = nlh->nlmsg_pid; /* pid of sending process */
+
+    // create reply
+    skb_out = nlmsg_new(msg_size, 0);
+    if (!skb_out) {
+      printk(KERN_ERR "netlink_test: Failed to allocate new skb\n");
+      return;
+    }
+
+    // put received message into reply
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    NETLINK_CB(skb_out).dst_group = 1; /* in multicast group */
+    strncpy(nlmsg_data(nlh), msg, msg_size);
+
+    res = netlink_broadcast(nl_sock, skb_out, 0, 1, GFP_KERNEL);
+    if (res < 0)
+      printk(KERN_INFO "netlink_test: Error while sending skb to user\n");
+}
+
+static int __init netlink_test_init(void)
+{
+  printk(KERN_INFO "netlink_test: Init module\n");
+
+  struct netlink_kernel_cfg cfg = {
+    .input = netlink_test_recv_msg,
+  };
+
+  nl_sock = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
+  if (!nl_sock) {
+    printk(KERN_ALERT "netlink_test: Error creating socket.\n");
+    return -10;
+  }
+
+  return 0;
+}
+
+static void __exit netlink_test_exit(void)
+{
+  printk(KERN_INFO "netlink_test: Exit module\n");
+
+  netlink_kernel_release(nl_sock);
+}
+
+module_init(netlink_test_init);
+module_exit(netlink_test_exit);
+
+MODULE_LICENSE("GPL");
--- multicast_example/nl_recv.c
+++ multicast_example/nl_recv.c
@@ -1,31 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
 #include <sys/socket.h>
 #include <linux/netlink.h>
 
-#define MAX_PAYLOAD 1024  /* maximum payload size*/
-struct sockaddr_nl src_addr, dest_addr;
-struct nlmsghdr *nlh = NULL;
-struct iovec iov;
-int sock_fd;
+#define MAX_PAYLOAD 1024  /* maximum payload size */
+#define NETLINK_TEST 17
 
-void main() {
-  sock_fd=socket(PF_NETLINK, SOCK_RAW, NETLINK_TEST);
 
-  memset(&src_addr, 0, sizeof(local_addr));
+int main()
+{
+  struct sockaddr_nl src_addr, dest_addr;
+  struct nlmsghdr *nlh;
+  struct msghdr msg;
+  struct iovec iov;
+  int sock_fd;
+
+  sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_TEST);
+
+  memset(&src_addr, 0, sizeof(src_addr));
   src_addr.nl_family = AF_NETLINK;
   src_addr.nl_pid = getpid();  /* self pid */
   /* interested in group 1<<0 */
   src_addr.nl_groups = 1;
-  bind(sock_fd, (struct sockaddr*)&src_addr,
-      sizeof(src_addr));
+  bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
 
   memset(&dest_addr, 0, sizeof(dest_addr));
 
-  nlh = (struct nlmsghdr *)malloc(
-                          NLMSG_SPACE(MAX_PAYLOAD));
+  nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
   memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
 
+  memset(&iov, 0, sizeof(iov));
   iov.iov_base = (void *)nlh;
   iov.iov_len = NLMSG_SPACE(MAX_PAYLOAD);
+
   msg.msg_name = (void *)&dest_addr;
   msg.msg_namelen = sizeof(dest_addr);
   msg.msg_iov = &iov;
@@ -34,8 +45,10 @@ void main() {
   printf("Waiting for message from kernel\n");
 
   /* Read message from kernel */
-  recvmsg(fd, &msg, 0);
-  printf("Received message payload: %s\n",
-        NLMSG_DATA(nlh));
+  recvmsg(sock_fd, &msg, 0);
+  printf("Received message payload: %s\n", NLMSG_DATA(nlh));
+
   close(sock_fd);
+
+  return 0;
 }
